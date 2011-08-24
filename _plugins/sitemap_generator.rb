71ab2aa685557a2301ff455da60d2ed65bd35c3b
Tasos Latsas <tlatsas2000@gmail.com>
2011-08-24 17:23:51 +0300

    update url for sitemap generator

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -39,7 +39,8 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://tlatsas.github.com"
+  #MY_URL = "http://tlatsas.github.com"
+  MY_URL = "http://www.kodama.gr"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
